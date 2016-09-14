6e314c78f1e7429154c7ea302382fbe9d6a39508
hitomiyamada <yamabon@me.com>
2016-09-14 14:26:14 +0000

    rewrite linkmenu.js

--- js/linkmenu.js
+++ js/linkmenu.js
@@ -1,21 +1,21 @@
 /*! サブメニュー */
 $(document).ready(function(){
 
-  $("#page_link h4").click(function () { // id="PAGE_LINK h4"を押したら実行
-    if($(this).hasClass("openlink")){ // h4にclass="openlink"があるかないか
-      $(this).removeClass("openlink"); // classにopenlinkがついている時は、それを削除
+  $("#page_link h4").click(function () {
+    if($(this).hasClass("openlink")){
+      $(this).removeClass("openlink");
     }else{
-      $(this).addClass("openlink"); // classにopenlinkが無い時はopenlinkを追加
+      $(this).addClass("openlink");
     }
-      $("#page_link h4 + ul").slideToggle(); // id="PAGE_LINK"の後にあるh4直後のULを表示させる
+      $("#page_link h4 + ul").slideToggle();
   });
 
-  $("#categories_link h4").click(function () { //id="CATEGORIES_LINK h4"を押したら実行
-    if($(this).hasClass("openlink")){ // h4にclass="openlink"があるかないか
-        $(this).removeClass("openlink"); // classにopenlinkがついている時は、それを削除
+  $("#categories_link h4").click(function () { /
+    if($(this).hasClass("openlink")){
+        $(this).removeClass("openlink");
     }else{
-      $(this).addClass("openlink"); // classにopenlinkが無い時はopenlinkを追加
+      $(this).addClass("openlink");
     }
-      $("#categories_link h4 + ul").slideToggle(); // id="CATEGORIES_LINK"の後にあるh4直後のULを表示させる
+      $("#categories_link h4 + ul").slideToggle();
   });
 });
